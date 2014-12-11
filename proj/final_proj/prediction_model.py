97172b4b2ff27f79ac1c6651f970a6c1452d0b6a
xz1082 <xz1082@nyu.edu>
2014-12-10 23:40:50 -0500

    modified prediction_model again

--- proj/final_proj/prediction_model.py
+++ proj/final_proj/prediction_model.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 from data_clean import *
 from sklearn.ensemble import RandomForestClassifier
 
@@ -7,13 +5,13 @@ from sklearn.ensemble import RandomForestClassifier
 def randomForest(pred_data):
     '''
     this function takes a panda serie of input data, builds a Random Forest model on our census data, and outputs the target value calculated via the Random Forest model
+    the Random Forest model excludes native-country, sex, race, workclass because they do not have significant feature importance    
     '''
-    model_data = clean_data_for_prediction('adult_data.txt')
+    model_data = clean_data_for_prediction(file_path)
     rf = RandomForestClassifier(n_estimators=150, min_samples_split=1)
-    rf.fit(model_data.drop('y', axis =1), model_data['y']) 
+    rf.fit(model_data.drop(['y', 'native-country', 'sex', 'race', 'workclass'], axis =1), model_data['y']) 
     income = rf.predict(pred_data)
     return income
   
 if __name__ == '__main__':
-    randomForest()
-    
+    randomForest(pred_data)
